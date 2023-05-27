40e8e1fb74befc5e70a60cf17c58cdf0c7c89ab9
Abdela Umer <adheremo65@gmail.com>
2023-05-26 22:12:12 -0700

    List and key lab challang

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const each = links.map((item,index)=>{
+return <a key = {index} href={`#${item}`}>{item} </a>
+    console.log(item)
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{each}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,17 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+   console.log(projects);
+  const each = projects.map((item)=>{
+    return < ProjectItem key = {item.id} id = {item.id} name = {item.name} about={item.about} technologies={item.technologies}  />
+    console.log(each)
+    
+   
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{each}</div>
     </div>
   );
 }
